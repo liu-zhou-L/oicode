e82e0ab1368ff9026085ff49308495b1ce0be249
123456 <liu_zhou_zhou@163.com>
2020-08-23 18:16:17 +0800

    hhh

---
+++ 八中编程社/【新初二必做】倍增【暑假集训】/天才ACM.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int n, m, t, k;
+long long a[MAXN];
+
+int main() {
+	scanf("%d", &k);
+	while (k--) {
+		scanf("%d%d%d", &n, &m, &t);
+		for (int i = 1; i <= n; i++) {
+			scanf("%lld", &a[i]);
+		}
+		int left, right;
+		left = right = old_r = 1;
+		b[left] = a[left];
+		while (left <= n) {
+			
+		}
+	}
+	return 0;
+}
