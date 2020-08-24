87ca69367932778b44784ef9912de499e1d8109c
liuzhoulin <liu_zhou_zhou@163.com>
2020-08-24 14:15:44 +0800

    push
    Signed-off-by: liuzhoulin <liu_zhou_zhou@163.com>

---
+++ 八中编程社/【新初二必做】RMQ【暑假集训】/数列区间最大值.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+const int MAXN = 1e5 + 5;
+
+int n, m, a[MAXN];
+
+int main() {
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i ,= n; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = 1; i <= m; i++) {
+		int tmp, tmp_;
+		
+	} 
+	return 0;
+} 
